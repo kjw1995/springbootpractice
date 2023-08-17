a9e1d54bc9c354e3ef5b428a65e3434c7e23c0f1
https://github.com/itshuproject/itshu.git <rohawell@daum.net>
2023-08-18 03:09:10 +0900

    first commit

---
+++ src/main/java/com/springbootpractice/springbootpractice/configuration/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.springbootpractice.springbootpractice.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        /*
+         * csrf 비활성화
+         */
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+    
+}
