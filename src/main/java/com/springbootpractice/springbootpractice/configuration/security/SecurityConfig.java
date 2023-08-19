a2a8624a1ac3ce5b88fbc33d0ee9f7a3abed25d7
https://github.com/itshuproject/itshu.git <rohawell@daum.net>
2023-08-20 02:56:47 +0900

    2023.08.20 - templates 경로 설정 및 메인,로그인 설정

--- src/main/java/com/springbootpractice/springbootpractice/configuration/security/SecurityConfig.java
+++ src/main/java/com/springbootpractice/springbootpractice/configuration/security/SecurityConfig.java
@@ -4,22 +4,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector HandlerMappingIntrospector) throws Exception {
         
         /*
          * csrf 비활성화
          */
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf((csrf) -> csrf.disable());
+
+    
+        /*
+         * 커스텀 로그인 방식
+         */
+        http.formLogin((formLogin) -> formLogin
+                                    .usernameParameter("userId")                                    
+                                    .passwordParameter("userPw")
+                                    .loginPage("/login.html")
+                                    .failureUrl("/login/fail")
+                                    .loginProcessingUrl("/login/process")
+        );
+
+        /*
+         * 로그아웃 설정
+         */
+        http.logout((logout) -> logout
+                                .deleteCookies("remove")
+                                .invalidateHttpSession(true)
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/main")
+        );
+            
 
         return http.build();
+
     }
-    
+
 }
---
+++ src/main/java/com/springbootpractice/springbootpractice/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.springbootpractice.springbootpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    
+
+    @GetMapping("/main")
+    public String goMain() {
+        return "main";
+    }
+
+
+}
